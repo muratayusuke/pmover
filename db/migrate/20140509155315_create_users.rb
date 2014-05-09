f4737752de0a929d09f00a070b048d01fb58aaec
Yukio Goto <yukio.goto@mail.rakuten.com>
2014-05-10 00:01:41 +0800

    add users table

---
+++ db/migrate/20140509155315_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users, id: false do |t|
+      t.string :provider, null: false
+      t.string :uid, null: false
+      t.string :email, null: true
+
+      t.timestamps
+    end
+
+    add_index :users, [:provider, :uid], unique: true
+  end
+end
